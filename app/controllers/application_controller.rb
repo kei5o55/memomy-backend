055ea3270221ab4c88294db56ada4bda0aa3219d
kei5o55 <uttk2003@gmail.com>
2026-09-16 07:17:11 +0900

    Add Render deployment config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: -> { Rails.env.production? && ENV["BASIC_AUTH_USER"].present? }
 
   protected
 
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :bio, :bgm_url, :icon ])
     # アカウント更新時に name 等を許可
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :bio, :bgm_url, :icon ])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end

5db23c3812fde36fc48a17fa46c9046d2a672e4e
kei5o55 <uttk2003@gmail.com>
2026-09-16 07:31:14 +0900

    fubocop

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# APIモードではアセットのコンパイルが不要なためコメントアウト/削除します
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+
 bundle exec rails db:migrate

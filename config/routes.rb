9778e1239414e649b00d488fedeb67d380b7e2c8
kei5o55 <uttk2003@gmail.com>
2026-09-16 16:17:21 +0900

    今度こそつながってくれたのむから

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #  }
   namespace :api do
     namespace :v1 do
-      get "health", to: "health#index"
+      get "health", to: "health#show"
       get "me", to: "users#me"
       get "test", to: "users#test"
       post "sync/import", to: "sync#import"
